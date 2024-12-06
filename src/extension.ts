cf6ae0461770f2339588fb4493fe35525cb09866
Matt Smith <matt@twobitfool.com>
2024-12-06 13:41:58 -0500

    Fire some code when the document has changed

--- src/extension.ts
+++ src/extension.ts
@@ -2,13 +2,18 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
+let verboseLogging = false;
+
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
+	// Set the logging level based on whether the extension is in development mode
+	verboseLogging = context.extensionMode === vscode.ExtensionMode.Development;
+
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "english-kiss" is now active!');
+	debug('Congratulations, your extension "english-kiss" is now active!');
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
@@ -20,7 +25,19 @@ export function activate(context: vscode.ExtensionContext) {
 	});
 
 	context.subscriptions.push(disposable);
+
+	vscode.workspace.onDidChangeTextDocument(event => {
+		debug('onDidChangeTextDocument fired');
+	});
 }
 
 // This method is called when your extension is deactivated
 export function deactivate() {}
+
+
+
+function debug(...args: any[]) {
+	if (verboseLogging) {
+		console.log('DEBUG', new Date().valueOf(), args.join('\t'));
+	}
+}
